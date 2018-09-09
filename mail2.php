c0611c0710c47f57c13a41e0f39d0cadf65ba078
Zel <zelaldogal@icloud.com>
2018-09-09 21:03:14 +0100

    Random

--- mail2.php
+++ mail2.php
@@ -1,6 +1,6 @@
 <?php 
 
-	$to = ""; // this is your Email address
+	$to = "sakshatbaral@gmail.com"; // this is your Email address
 	$from  = $_POST['email']; // this is the sender's Email address
 	$sender_name = $_POST['name'];
 	$phone = $_POST['phone'];
